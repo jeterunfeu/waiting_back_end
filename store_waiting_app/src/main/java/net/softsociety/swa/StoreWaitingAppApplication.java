79ec387e08a938fbb49f2173fd858187bf09ab15
SES-2303\user <jeterunfeu@gmail.com>
2023-04-24 15:51:45 +0900

    initialization

---
+++ store_waiting_app/src/main/java/net/softsociety/swa/StoreWaitingAppApplication.java
@@ -0,0 +1,13 @@
+package net.softsociety.swa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreWaitingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreWaitingAppApplication.class, args);
+	}
+
+}
---
+++ store_waiting_app/src/test/java/net/softsociety/swa/StoreWaitingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.softsociety.swa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreWaitingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
